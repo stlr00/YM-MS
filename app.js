49de784c31eb8a8f16b39e84df50d209b177b2ba
Alexander Vesyolkin <sashaasdzxc@gmail.com>
2022-07-21 13:19:23 +0300

    fixes for stability

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-setTimeout(setup, 1000)
+setTimeout(setup, 1500)
 
 function setup() {
     navigator.mediaSession.setActionHandler('play', () => externalAPI.togglePause(false))
@@ -10,21 +10,26 @@ function setup() {
 }
 
 function setMetaData() {
-    const track = externalAPI.getCurrentTrack()
-    const artist = track.artists.map(art => art.title).join(', ')
+    try {
+        const track = externalAPI.getCurrentTrack()
+        const artist = track.artists.map(art => art.title).join(', ')
 
-    navigator.mediaSession.metadata = new MediaMetadata({
-        title: track.title,
-        album: track.album.title,
-        artist: artist,
-        artwork: [
-            {src: 'https://' + track.cover.replace('%%', '30x30'), sizes: '30x30', type: 'image/png'},
-            {src: 'https://' + track.cover.replace('%%', '50x50'), sizes: '50x50', type: 'image/png'},
-            {src: 'https://' + track.cover.replace('%%', '80x80'), sizes: '80x80', type: 'image/png'},
-            {src: 'https://' + track.cover.replace('%%', '100x100'), sizes: '100x100', type: 'image/png'},
-            {src: 'https://' + track.cover.replace('%%', '200x200'), sizes: '200x200', type: 'image/png'},
-            {src: 'https://' + track.cover.replace('%%', '300x300'), sizes: '300x300', type: 'image/png'},
-            {src: 'https://' + track.cover.replace('%%', '400x400'), sizes: '400x400', type: 'image/png'},
-        ],
-    })
+        navigator.mediaSession.metadata = new MediaMetadata({
+            title: track.title,
+            album: track.album.title,
+            artist: artist,
+            artwork: [
+                {src: 'https://' + track.cover.replace('%%', '30x30'), sizes: '30x30', type: 'image/png'},
+                {src: 'https://' + track.cover.replace('%%', '50x50'), sizes: '50x50', type: 'image/png'},
+                {src: 'https://' + track.cover.replace('%%', '80x80'), sizes: '80x80', type: 'image/png'},
+                {src: 'https://' + track.cover.replace('%%', '100x100'), sizes: '100x100', type: 'image/png'},
+                {src: 'https://' + track.cover.replace('%%', '200x200'), sizes: '200x200', type: 'image/png'},
+                {src: 'https://' + track.cover.replace('%%', '300x300'), sizes: '300x300', type: 'image/png'},
+                {src: 'https://' + track.cover.replace('%%', '400x400'), sizes: '400x400', type: 'image/png'},
+            ],
+        })
+    } catch (e) {
+        setTimeout(setMetaData, 300)
+    }
+
 }
